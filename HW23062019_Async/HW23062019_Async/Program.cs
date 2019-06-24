5c0cdbc185f025a8f91cb1ec1e9556962bf000cc
ruslan261081 <45044693+ruslan261081@users.noreply.github.com>
2019-06-24 22:04:06 +0300

    Add files via upload

---
+++ HW23062019_Async/HW23062019_Async/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HW23062019_Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double> funcPie = new Func<double>(ReturnPie);
+            IAsyncResult asyncResult = funcPie.BeginInvoke((IAsyncResult ar) =>
+            {
+                Console.WriteLine("after..........");
+
+                Console.WriteLine(ar.AsyncState);
+
+                double res = funcPie.EndInvoke(ar);
+                Console.WriteLine("what was the result? " + res);
+            }, "hello");
+            Thread.Sleep(3000);
+        }
+        static double ReturnPie()
+        {
+            return 3.14;
+        }
+    }
+}
---
+++ HW23062019_Async2/HW23062019_Async2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HW23062019_Async2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string> funcString = new Func<string>(ReturnString);
+            funcString += ReturnString;
+            funcString += ReturnString;
+
+            string AllString = "", res = "";
+            foreach(Func<string> method in funcString.GetInvocationList())
+            {
+                IAsyncResult async = method.BeginInvoke((IAsyncResult ar) =>
+                {
+                    res = method.EndInvoke(ar);
+                    AllString += res;
+                }, null);
+
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine($"Result is: {AllString}");
+        }
+        static string ReturnString()
+        {
+            return "I Belive I Can Fly  ";
+        }
+    }
+}
